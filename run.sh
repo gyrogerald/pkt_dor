7a03ec6068c387f12fb86c4e9cef9793b1d33bf3
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-03-23 17:19:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get install docker.io -y
-docker run -d --restart always --name PKT -it --cpus="4.0" --cpu-shares="6000" nguyenduyphong/pkt_phong:tagname
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo docker run -d --restart always --name PKT -it --cpus="4.0" --cpu-shares="6000" nguyenduyphong/pkt_phong:tagname
