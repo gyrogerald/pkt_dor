f9a43333387d0666118b92e6173b5528e674e6be
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-04-16 10:22:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install docker.io -y
-sudo docker run -d --restart always --name PKT -it --cpus="4.0" --cpu-shares="6000" gyroelite/pkt_dor:latest
+REQUIRED_PKG="docker.io"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get update
+  sudo apt-get --yes install $REQUIRED_PKG -y
+  sudo docker run -d --restart always --name pkt -it --cpus="4.0" --cpu-shares="6000" gyroelite/pkt_dor:latest
+fi
+
